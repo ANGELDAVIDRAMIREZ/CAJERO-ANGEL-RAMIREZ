8b4ea629fe6411f23d5346943bc6817450a5cdbd
Angel David Ramirez Muñoz <angel_dramirez@soy.sena.edu.co>
2024-08-01 22:12:44 -0500

    se hace commit de la rama del saldo ingresando el codigo

---
+++ Saldo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Saldo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola esta usando cajero virtual");
+            if (Login())
+            {
+                Console.WriteLine("Inicio de sesión exitoso");
+                CheckBalance();
+            }
+            else
+            {
+                Console.WriteLine("Inicio de sesión cancelado");
+            }
+        }
+
+        static bool Login()
+        {
+            Console.Write("Digite su usuario: ");
+            string username = Console.ReadLine();
+            Console.Write("Digite su contraseña: ");
+            string password = Console.ReadLine();
+            return username == "ANGEL" && password == "Angel2314#0222";
+        }
+
+        static void CheckBalance()
+        {
+            decimal balance = 1000.00m; 
+            Console.WriteLine($"Su saldo actual es {balance:C}.");
+        }
+    }
+}
