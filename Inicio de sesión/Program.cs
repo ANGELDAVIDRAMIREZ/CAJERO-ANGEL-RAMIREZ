0f08b040cae37748b42fd3d036e8592edeaab77b
Angel David Ramirez Muñoz <angel_dramirez@soy.sena.edu.co>
2024-08-01 22:02:40 -0500

    Se hace commit de la rama de sesion con el codigo

---
+++ Inicio de sesión/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace sesion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola bienvenido al cajero");
+            if (Login())
+            {
+                Console.WriteLine("Inicio de sesión completado");
+            }
+            else
+            {
+                Console.WriteLine("Inicio de sesión cancelado");
+            }
+        }
+
+        static bool Login()
+        {
+            Console.Write("Digite su usuario: ");
+            string username = Console.ReadLine();
+            Console.Write("Digite su contraseña: ");
+            string password = Console.ReadLine();
+
+            return username == "ANGEL" && password == "Angel2314#0222"; 
+        }
+    }
+}
