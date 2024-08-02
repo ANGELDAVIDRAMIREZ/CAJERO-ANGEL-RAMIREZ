b27babfc71f010ecbc7560305d2477cbedb25efb
Angel David Ramirez Muñoz <angel_dramirez@soy.sena.edu.co>
2024-08-01 22:15:18 -0500

    se hace commit en la rama ingresando el codigo

---
+++ Transferencia/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Transferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola esta usando cajero virtual");
+            if (Login())
+            {
+                Console.WriteLine("Inicio de sesión exitoso");
+                Transfer();
+            }
+            else
+            {
+                Console.WriteLine("Inicio de sesión cancelado");
+            }
+        }
+
+        static bool Login()
+        {
+            Console.Write("Digite su usuario: ");
+            string username = Console.ReadLine();
+            Console.Write("Digite su contraseña: ");
+            string password = Console.ReadLine();
+            return username == "admin" && password == "1234";
+        }
+
+        static void Transfer()
+        {
+            Console.Write("cantidad a transferir: ");
+            decimal amount = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Ingrese el número de cuenta destinatario: ");
+            string accountNumber = Console.ReadLine();
+            Console.WriteLine($"Has transferido {amount:C} a la cuenta {accountNumber}.");
+        }
+    }
+}
