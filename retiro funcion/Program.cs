87fa381093cbc683bd7654926f8323a6bdf25b54
Angel David Ramirez Muñoz <angel_dramirez@soy.sena.edu.co>
2024-08-01 22:07:06 -0500

    se hace commit en la rama func/retiro ingresando el codigo

---
+++ retiro funcion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RETIRAR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola estas usando cajero virtual");
+            if (Login())
+            {
+                Console.WriteLine("Inicio de sesión exitoso");
+                Withdraw();
+            }
+            else
+            {
+                Console.WriteLine("Inicio de sesión cancelado");
+            }
+        }
+
+        static bool Login()
+        {
+            Console.Write("Ingrese su usuario: ");
+            string username = Console.ReadLine();
+            Console.Write("Ingrese su contraseña: ");
+            string password = Console.ReadLine();
+            return username == "ANGEL" && password == "Angel2314#0222";
+        }
+
+        static void Withdraw()
+        {
+            Console.Write("dinero a retirar: ");
+            decimal amount = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine($"Has retirado {amount:C}.");
+        }
+    }
+}
